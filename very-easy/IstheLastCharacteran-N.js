ee3f69d16b2666f081744c88a73aa5fb3fc09533
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-08-22 18:18:10 +0300

    feat: add very Easy :Is the Last Character an N?[Regex]

---
+++ very-easy/IstheLastCharacteran-N.js
@@ -0,0 +1,7 @@
+function isLastCharacterN(word) {
+  regexn = /n$/;
+  isN = regexn.test(word);
+  return isN;
+}
+
+console.log("🚀 ~ isLastCharacterN ~ isLastCharacterN:", isLastCharacterN(mohaned));
