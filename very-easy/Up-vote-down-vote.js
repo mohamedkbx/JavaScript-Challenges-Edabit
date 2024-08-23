a97803524d6f13cf10346fcff3a9dbd991fe84fb
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-08-23 20:35:20 +0300

    feat : up-vor-downvote (object extraction)

---
+++ very-easy/Up-vote-down-vote.js
@@ -0,0 +1,13 @@
+function getVoteCount(votes) {
+  return votes.upvotes - votes.downvotes;
+}
+
+// Upvotes vs Downvotes
+// Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+// Examples
+// getVoteCount({ upvotes: 13, downvotes: 0 }) ➞ 13
+
+// getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
+
+// getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
