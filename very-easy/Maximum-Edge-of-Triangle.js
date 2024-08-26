c943b9e22dfab2063de72f8b7f2d70e8fd8de576
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-08-26 21:26:40 +0300

    feat: add function to calculate maximum edge of a triangle

---
+++ very-easy/Maximum-Edge-of-Triangle.js
@@ -0,0 +1,8 @@
+function nextEdge(side1, side2) {
+  return side1 + side2 - 1;
+}
+
+// Notes
+// (side1 + side2) - 1 = maximum range of third edge.
+// The side lengths of the triangle are positive integers.
+// Don't forget to return the result.
